de633bd3b071946a4efe3f5fee69275630a69b75
sszakony <sszakony@nd.edu>
2016-03-18 15:14:16 -0400

    Added structure to parsingUtility.py

--- sc/parsingUtility.py
+++ sc/parsingUtility.py
@@ -5,3 +5,60 @@ class parsingUtility:
     def __init__(self):
         pass
 
+    def parseCommand(self,cmdString):
+        #Main entry point
+        pass
+
+    def getCommand(self, cmdString):
+        #returns command from cmdString
+        pass
+
+    def parseFROM(self, cmdFROM):
+        pass
+
+    def parseMAINTAINER(self, cmdMAINTAINER):
+        pass
+
+    def parseRUN(self, cmdRUN):
+        pass
+
+    def parseCMD(self,cmdCMD):
+        pass
+
+    def parseLABEL(self, cmdLABEL):
+        pass
+
+    def parseEXPOSE(self, cmdEXPOSE):
+        pass
+
+    def parseENV(self, cmdENV):
+        pass
+
+    def parseADD(self, cmdADD):
+        pass
+
+    def parseCOPY(self,cmdCOPY):
+        pass
+
+    def parseENTRYPOINT(self, cmdENTRYPOINT):
+        pass
+
+    def parseVOLUME(self, cmdVOLUME):
+        pass
+
+    def parseUSER(self, cmdUSER):
+        pass
+
+    def parseWORKDIR(self, cmdWORKDIR):
+        pass
+
+    def parseARG(self, cmdARG):
+        pass
+
+    def parseONBUILD(self, cmdONBUILD):
+        pass
+
+    def parseSTOPSIGNAL(self, cmdSTOPSIGNAL):
+        pass
+
+
