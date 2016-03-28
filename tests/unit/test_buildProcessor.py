62a24d8e13f468dc61ae3a5cfee6d6f0459a833f
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2016-03-28 16:10:54 -0400

    Add openmalaria Dockerfile to unit test data directory.

--- tests/unit/test_buildProcessor.py
+++ tests/unit/test_buildProcessor.py
@@ -15,10 +15,20 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 class BuildProcessorTestCase(unittest.TestCase):
     def setUp(self):
-        self.build_processor = buildProcessor.buildProcessor()
-        self.result = self.build_processor.processDF(os.path.join(base_dir, 'data/Dockerfile.txt'))
+        self.processors = [
+            buildProcessor.buildProcessor(),
+            buildProcessor.buildProcessor()
+        ]
+        self.results = []
+
+        self.results.append(self.processors[0].processDF(os.path.join(
+            base_dir, 'data/Dockerfile.txt')))
+
+        self.results.append(self.processors[1].processDF(os.path.join(
+            base_dir, 'data/openmalaria/Dockerfile')))
 
     def test_processDF(self):
         # Process the test data file, and check that the return is 0,
         #  indicating that no errors were encountered.
-        self.assertEqual(self.result, 0)
+        self.assertEqual(self.results[0], 0)
+        self.assertEqual(self.results[1], 0)
