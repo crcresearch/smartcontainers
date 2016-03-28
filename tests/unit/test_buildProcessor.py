2a3b4589e4d08f9a3fea06e672fff1308c8fec05
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2016-03-28 13:54:26 -0400

    Utilize os.path methods for finding test Dockerfile.

--- tests/unit/test_buildProcessor.py
+++ tests/unit/test_buildProcessor.py
@@ -10,10 +10,13 @@ import unittest
 from sc import buildProcessor
 
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
 class BuildProcessorTestCase(unittest.TestCase):
     def setUp(self):
         self.build_processor = buildProcessor.buildProcessor()
-        self.result = self.build_processor.processDF('unit/data/Dockerfile.txt')
+        self.result = self.build_processor.processDF(os.path.join(base_dir, 'data/Dockerfile.txt'))
 
     def test_processDF(self):
         # Process the test data file, and check that the return is 0,
