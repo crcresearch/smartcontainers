2c30e0ec16d818f7cd7f26bc4b2d5a2f159876a3
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2016-03-30 16:47:19 -0400

    Utilize native unittest module.

--- tests/unit/test_buildProcessor.py
+++ tests/unit/test_buildProcessor.py
@@ -5,11 +5,17 @@ Tests for Smart Containers buildProcessor.
 This module provides functions for processing Build commands.
 """
 import os
-from sc import buildProcessor
-P = buildProcessor.buildProcessor()
+import unittest
 
-def test_processDF():
-    #Process the test data file, and check that the return is 0,
-    #indicating that no errors were encountered.
-    result = P.processDF('unit/data/Dockerfile.txt')
-    assert result == 0
+from sc import buildProcessor
+
+
+class BuildProcessorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.build_processor = buildProcessor.buildProcessor()
+        self.result = self.build_processor.processDF('unit/data/Dockerfile.txt')
+
+    def test_processDF(self):
+        # Process the test data file, and check that the return is 0,
+        #  indicating that no errors were encountered.
+        self.assertEqual(self.result, 0)
